32ff9390340b512d2800253179f0ba4340778419
Ana Cortez <anacarolcortez@hotmail.com>
2022-12-25 08:44:42 -0300

    Delete dscatalog/target directory

--- dscatalog/target/classes/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO categories (name, created_At) VALUES('Books', NOW());
-INSERT INTO categories (name, created_At) VALUES('Electronics', NOW());
-INSERT INTO categories (name, created_At) VALUES('Accessories', NOW());
-INSERT INTO categories (name, created_At) VALUES('Computers', NOW());
