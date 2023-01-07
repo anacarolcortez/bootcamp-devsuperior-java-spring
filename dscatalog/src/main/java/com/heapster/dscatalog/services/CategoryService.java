db802e3b26d9b7aef6ffbd55a81cb1a32f8298cb
anacarolcortez <anacarolcortez@hotmail.com>
2023-01-07 14:24:59 -0300

    limpeza imports

--- dscatalog/src/main/java/com/heapster/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/heapster/dscatalog/services/CategoryService.java
@@ -9,15 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
