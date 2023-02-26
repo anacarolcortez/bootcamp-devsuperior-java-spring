004af667f6f4100805ebb28fa03e2f1c87bf62b2
anacarolcortez <anacarolcortez@hotmail.com>
2023-02-26 19:16:14 -0300

    email unico usuario

--- dscatalog/src/main/java/com/heapster/dscatalog/entities/User.java
+++ dscatalog/src/main/java/com/heapster/dscatalog/entities/User.java
@@ -16,6 +16,7 @@ public class User implements Serializable {
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String password;
 
