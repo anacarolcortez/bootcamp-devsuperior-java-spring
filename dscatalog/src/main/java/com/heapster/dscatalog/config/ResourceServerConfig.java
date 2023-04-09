f505ef68be5488c36404b9b1e81ca90a49de479e
anacarolcortez <anacarolcortez@hotmail.com>
2023-04-09 09:14:06 -0300

    config h2 test security

--- dscatalog/src/main/java/com/heapster/dscatalog/config/ResourceServerConfig.java
+++ dscatalog/src/main/java/com/heapster/dscatalog/config/ResourceServerConfig.java
@@ -2,12 +2,15 @@ package com.heapster.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -16,7 +19,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
     private static final String[] ADMIN = {"/users/**"};
 
@@ -27,6 +33,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //Config para H2 (teste)
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
