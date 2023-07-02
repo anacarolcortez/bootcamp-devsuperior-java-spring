a01cfb4a04d16cc287880058d5799a2087ddbcdb
anacarolcortez <anacarolcortez@hotmail.com>
2023-07-02 20:39:14 -0300

    projeto 2 - orm

---
+++ dslearn/src/test/java/com/bootcamp/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
