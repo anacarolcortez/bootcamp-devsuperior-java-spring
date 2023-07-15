c934f519110888321385e4ee1530bc97a809c5a1
anacarolcortez <anacarolcortez@hotmail.com>
2023-07-15 12:52:31 -0300

    exclusao duplicidade

--- catalog/src/main/java/com/javaspringclass/catalog/repository/GameRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javaspringclass.catalog.repository;
-
-import com.javaspringclass.catalog.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
